a0195a119d411f79c79d8e650a68c15358c71cc6
John Ramey <johnramey@gmail.com>
2011-07-06 17:22:38 -0700

    Updated GUI with option to query oracle for true labels (at random). Added handler function for this. Right now, querying the oracle breaks because all of the observations are already labeled.

--- GUI_Active_Learning.r
+++ GUI_Active_Learning.r
@@ -1,8 +1,8 @@
 library('ProjectTemplate')
 load.project()
 
-updatePlot <- function(h,...) {
-	data <- gen_data(
+h_gen_data <- function(h,...) {
+	data <<- gen_data(
 		n = svalue(rdo_sample_sizes),
 		num_groups = svalue(cbo_num_groups),
 		shape = svalue(rdo_shapes),
@@ -10,6 +10,14 @@ updatePlot <- function(h,...) {
 	plot_bivariate(data$X1, data$X2, data$y)
 }
 
+h_query_oracle <- function(h, ...) {
+	print(svalue(cbo_query_methods))
+	oracle_out <- active_learn(data = data, method = tolower(svalue(cbo_query_methods)))
+	data <- oracle_out$data
+	plot_bivariate(data$X1, data$X2, data$y)
+}
+
+# Constants to generate data.
 sample_sizes <- c(50, 100, 200, 300)
 num_groups <- seq.int(2, 5)
 shapes <- c(
@@ -17,19 +25,22 @@ shapes <- c(
 	Low = "Low Correlation",
 	High = "High Correlation")
 query_methods <- c(Random="sample")
+num_query <- seq.int(1, 5)
 
-distance_slider <- gslider(from=0.01,to=10,by=.01, value=3, handler=updatePlot)
-cbo_num_groups <- gcombobox(num_groups, handler=updatePlot)
-rdo_sample_sizes <- gradio(sample_sizes, handler = updatePlot)
-rdo_shapes <- gradio(shapes, handler=updatePlot)
-	
-cbo_query_methods <- gcombobox(names(query_methods), handler=updatePlot)
+# GUI Controls.
+distance_slider <- gslider(from=0.01,to=10,by=.01, value=3, handler=h_gen_data)
+cbo_num_groups <- gcombobox(num_groups, handler=h_gen_data)
+rdo_sample_sizes <- gradio(sample_sizes, handler = h_gen_data)
+rdo_shapes <- gradio(shapes, handler=h_gen_data)	
+cbo_query_methods <- gcombobox(names(query_methods), handler=h_query_oracle)
+cbo_num_query <- gcombobox(num_query, handler=h_query_oracle)
 
-## now layout
+# Create the layout of the window.
 window <- gwindow("Active Learning Demo")
 BigGroup <- ggroup(cont=window)
 group <- ggroup(horizontal=FALSE, container=BigGroup)
 
+# Adds the GUI controls to the GUI.
 tmp <- gframe("Shape", container=group)
 add(tmp, rdo_shapes)
 tmp <- gframe("Number of Groups", container=group)
@@ -38,10 +49,13 @@ tmp <- gframe("Sample Size", container=group)
 add(tmp, rdo_sample_sizes)
 tmp <- gframe("Distance between Groups", container=group)
 add(tmp, distance_slider, expand=TRUE)
-
 tmp <- gframe("Query Methods", container=group)
 add(tmp, cbo_query_methods)
-
+add(tmp, cbo_num_query)
 
 # Now to add a graphics device.
 add(BigGroup, ggraphics())
+
+# I'm making 'data' global to make querying the oracle easier.
+# Yes, this is cheating and is bad practice.
+data <- NULL
