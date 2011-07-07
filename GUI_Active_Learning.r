6971d4822c9a572f01418096cc4a5103a9415571
John Ramey <johnramey@gmail.com>
2011-07-07 08:37:29 -0700

    Added option in GUI to label a percentage of the data when generated.

--- GUI_Active_Learning.r
+++ GUI_Active_Learning.r
@@ -2,11 +2,11 @@ library('ProjectTemplate')
 load.project()
 
 h_gen_data <- function(h,...) {
-	# TODO: Let user specify this. It must be greater than 1.
-	num_labeled_per_class <- 5
+	n <- svalue(rdo_sample_sizes)
+	num_labeled_per_class <- floor(n * svalue(rdo_pct_labeled) / 100)
 	data <<- gen_partial_labeled_data(
 		num_labeled_per_class = num_labeled_per_class,
-		n = svalue(rdo_sample_sizes),
+		n = n,
 		num_groups = svalue(cbo_num_groups),
 		shape = svalue(rdo_shapes),
 		dist = svalue(distance_slider))
@@ -24,6 +24,7 @@ h_query_oracle <- function(h, ...) {
 
 # Constants to generate data.
 sample_sizes <- c(50, 100, 200, 300)
+pct_labeled <- c(25, 50, 75)
 num_groups <- seq.int(2, 5)
 shapes <- c(
 	Spherical = "Spherical",
@@ -36,6 +37,7 @@ num_query <- seq.int(1, 5)
 distance_slider <- gslider(from=0.01,to=10,by=.01, value=3, handler=h_gen_data)
 cbo_num_groups <- gcombobox(num_groups, handler=h_gen_data)
 rdo_sample_sizes <- gradio(sample_sizes, handler = h_gen_data)
+rdo_pct_labeled <- gradio(pct_labeled, handler = h_gen_data)
 rdo_shapes <- gradio(shapes, handler=h_gen_data)	
 cbo_query_methods <- gcombobox(names(query_methods), handler=h_query_oracle)
 cbo_num_query <- gcombobox(num_query, handler=h_query_oracle)
@@ -52,6 +54,8 @@ tmp <- gframe("Number of Groups", container=group)
 add(tmp, cbo_num_groups)
 tmp <- gframe("Sample Size", container=group)
 add(tmp, rdo_sample_sizes)
+tmp <- gframe("Percentage of Data to Label", container=group)
+add(tmp, rdo_pct_labeled)
 tmp <- gframe("Distance between Groups", container=group)
 add(tmp, distance_slider, expand=TRUE)
 tmp <- gframe("Query Methods", container=group)
